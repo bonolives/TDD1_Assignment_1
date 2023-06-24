5c8ff2c2fa9ee6bbde73b905cdcc06de2a90efd9
olive <bon_olives@yahoo.com>
2023-06-25 02:05:20 +0300

    ADD TEST FIRST TDD CYCLE 1

---
+++ test.py
@@ -0,0 +1,15 @@
+# Test First TDD Cycle 1
+
+# Writing first test
+
+
+def test1_multiply():
+    assert multiply(1, 1) == 1
+
+
+# Writing a multiply function to get the test to pass
+
+
+def multiply(a, b):
+    return a * b
+

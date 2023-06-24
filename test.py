10c223fb344a57cfeba308df48d56508b6c66114
olive <bon_olives@yahoo.com>
2023-06-25 02:40:44 +0300

    ADD TEST FIRST TDD CYCLE 3 PASSED

--- test.py
+++ test.py
@@ -3,17 +3,17 @@
 # Writing first test
 
 
-#def test1_multiply():
-    #assert multiply(1, 1) == 1
+def test1_multiply():
+    assert multiply(1, 1) == 1
 
 
-'''
+"""
 Writing a multiply function to get the test to pass
 
 
 def multiply(a, b):
     return a * b
-'''
+"""
 
 
 # Test First TDD Cycle 2
@@ -21,19 +21,23 @@ def multiply(a, b):
 # writing Second Test
 
 
-#def test2_multiply():
-    #assert multiply(2, 2) == 4
+def test2_multiply():
+    assert multiply(2, 2) == 4
 
 
-'''
+"""
 updating the multiply function to pass test 2 but making sure test 1 passes too
 
 def multiply(a, b):
     return a * b
-'''
+"""
+
 
 # writing third test
 def test3_multiply():
-    assert multiply(3,3) == 9
+    assert multiply(3, 3) == 9
 
 
+# Updating multiply function making sure not to break test 1 and test 2
+def multiply(a, b):
+    return a * b

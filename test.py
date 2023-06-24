6bc28f32de0207832e9e073a336e9fdcfcde2295
olive <bon_olives@yahoo.com>
2023-06-25 02:34:34 +0300

    ADD TEST3 FAILED

--- test.py
+++ test.py
@@ -3,27 +3,37 @@
 # Writing first test
 
 
-def test1_multiply():
-    assert multiply(1, 1) == 1
+#def test1_multiply():
+    #assert multiply(1, 1) == 1
 
 
-# Writing a multiply function to get the test to pass
+'''
+Writing a multiply function to get the test to pass
 
 
-# def multiply(a, b):
-# return a * b
+def multiply(a, b):
+    return a * b
+'''
+
 
 # Test First TDD Cycle 2
 
 # writing Second Test
 
 
-def test2_multiply():
-    assert multiply(2, 2) == 4
+#def test2_multiply():
+    #assert multiply(2, 2) == 4
 
 
-# updating the multiply function to pass test 2 but making sure test 1 passes too
-
+'''
+updating the multiply function to pass test 2 but making sure test 1 passes too
 
 def multiply(a, b):
     return a * b
+'''
+
+# writing third test
+def test3_multiply():
+    assert multiply(3,3) == 9
+
+
